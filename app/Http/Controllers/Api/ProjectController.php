d67699a1c83e9720d070c75310b38dd5943e04f6
Shinrael <mircoelena89@gmail.com>
2024-06-03 16:56:43 +0200

    "Added ProjectController with index method and API route for /projects"

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = Project::with('technology', 'type');
+        return response()->json($projects);
+    }
+
 }
